3d1e42abc1e4ab3bc3e8ed6a3346957ae67e304f
vikash <choubeyvikash201996@gmail.com>
2026-02-07 21:38:43 +0530

    react dom setup

--- rst_ui/src/App.jsx
+++ rst_ui/src/App.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import Login from './pages/Login'
 import { Toaster } from 'react-hot-toast';
 import Register from './pages/Register';
+import {Route,Routes} from "react-router-dom"
 
 const App = () => {
   return (
     <div>
        <Toaster position="top-center" reverseOrder={false} />
-      {/* <Login/> */}
-      <Register/>
+       <Routes>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={ <Register/>} />
+       </Routes>
+        
     </div>
   )
 }
